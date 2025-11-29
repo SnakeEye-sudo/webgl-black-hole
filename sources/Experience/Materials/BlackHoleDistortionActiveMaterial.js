9f0ec94b0cd629618d15b5c07c2d08fdc614891e
SnakeEye-sudo (Er. Sangam Krishna) <krishna.sangam11rsm@gmail.com>
2025-11-29 17:49:58 +0530

    fix: Change GLSL version to GLSL1 for iOS WebGL compatibility
    The refraction effect was not working on iOS devices due to GLSL 3.0 not being fully supported on iOS WebGL implementations. Changed glslVersion from THREE.GLSL3 to THREE.GLSL1 to ensure better compatibility with iOS Safari and WebKit-based browsers.
    
    Fixes #1

--- sources/Experience/Materials/BlackHoleDistortionActiveMaterial.js
+++ sources/Experience/Materials/BlackHoleDistortionActiveMaterial.js
@@ -6,8 +6,7 @@ import fragmentShader from '../shaders/blackHoleDistortionActive/fragment.glsl'
 function BlackHoleDistortionActiveMaterial()
 {
     const material = new THREE.RawShaderMaterial({
-        glslVersion: THREE.GLSL3,
-        side: THREE.DoubleSide,
+glslVersion: THREE.GLSL1,        side: THREE.DoubleSide,
         // blending: THREE.AdditiveBlending,
         // depthWrite: false,
         // depthTest: false,
